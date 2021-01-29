267c5730ef60c611a0f6af4af53a81d51619b40d
David Young <roboguy06@gmail.com>
2021-01-29 02:53:39 -0600

    Totient chains

--- TotientChains.v
+++ TotientChains.v
@@ -2,6 +2,8 @@ Require Import Lia.
 Require Import Coq.Program.Equality.
 Require Import Coq.Arith.Wf_nat.
 Require Import Coq.Arith.Compare_dec.
+Require Import Coq.Arith.PeanoNat.
+Require Import Coq.Program.Wf.
 
 Inductive Parity := Even | Odd.
 
@@ -27,15 +29,6 @@ Ltac solve_Gcd :=
   unfold Gcd; split; [> split | idtac]; try lia;
   repeat (((apply Gcd_lt; [> lia | idtac]; simpl) || (apply Gcd_gt; [> lia | idtac]; simpl)));
   apply Gcd_refl.
-
-(*
-Ltac solve_notGcd :=
-  let H := fresh "H"
-  in
-  let G := fresh "G"
-  in
-  intro H; destruct H as (_ & G); inversion G; subst; lia.
-*)
 
 Ltac solve_notGcd :=
   let H := fresh "H"
@@ -78,7 +71,7 @@ Proof.
   apply IHg; lia.
 Defined.
 
-Theorem Gcd_is_fn : forall x y z z',
+Theorem Gcd_is_fn : forall {x y z z'},
   x > 0 -> y > 0 ->
   Gcd x y z ->
   Gcd x y z' ->
@@ -95,6 +88,62 @@ Proof.
   destruct g; try lia. assumption.
 Defined.
 
+(*Inductive Gcd' : nat -> nat -> nat -> Type :=
+| Gcd_refl : forall a, Gcd' a a a
+| Gcd_gt : forall a b x, a > b -> Gcd' (a - b) b x -> Gcd' a b x
+| Gcd_lt : forall a b x, a < b -> Gcd' a (b - a) x -> Gcd' a b x.*)
+
+Program Fixpoint Gcd'_exists x y {measure (x+y)} :
+  ({z & Gcd' x y z} + (x = 0) + (y = 0)) :=
+  match x, y with
+  | O, _ => _
+  | _, O => _
+  | S x', S y' => _
+  end.
+Next Obligation. left. right. reflexivity. Defined.
+Next Obligation. right. reflexivity. Defined.
+Next Obligation.
+  destruct (lt_dec (S x') (S y')).
+  - destruct (Gcd'_exists (S x') (S y' - S x')); try lia.
+    destruct s; try lia. destruct s.
+    left. left.
+    exists x.
+    apply Gcd_lt. assumption. assumption.
+
+  - destruct (Nat.eq_dec (S x') (S y')). left. left. rewrite e. exists (S y'). constructor.
+
+    destruct (Gcd'_exists (S x' - S y') (S y')); try lia.
+    destruct s. destruct s. left. left.
+    exists x. apply Gcd_gt; try lia. assumption.
+
+    lia.
+Defined.
+
+Theorem Gcd_exists : forall x y,
+  x > 0 -> y > 0 ->
+  {z & Gcd x y z}.
+Proof.
+  intros.
+  destruct (Gcd'_exists x y).
+  destruct s. destruct s. exists x0.
+  split; try split; try assumption. lia. lia.
+Defined.
+
+Theorem Gcd1_dec : forall x y,
+  (Gcd x y 1) + notT (Gcd x y 1).
+Proof.
+  intros.
+  destruct x, y; try (right; intro; destruct H; intuition; lia).
+  destruct (Gcd_exists (S x) (S y)); try lia.
+  destruct (Nat.eq_dec x0 1).
+  left. subst. assumption.
+  right. intro.
+  assert (A0 : S x > 0). lia.
+  assert (A1 : S y > 0). lia.
+  pose (P := Gcd_is_fn A0 A1 g H).
+  lia.
+Defined.
+
 
 Inductive Totient' : nat -> nat -> nat -> Type :=
 | Totient'_One : forall x, Totient' 1 x 1
@@ -105,18 +154,34 @@ Inductive Totient' : nat -> nat -> nat -> Type :=
 
 Definition Totient n t := Totient' n n t.
 
+(*
+Fixpoint totient (d n : nat) : nat.
+Proof.
+  Check wf.
+*)
 
+(*
 Ltac solveTotient :=
   repeat
-    (((apply Totient'_One)
+    ((apply Totient'_NotGcd1; [> solve_notGcd | idtac])
       ||
-     (apply Totient'_NotGcd1; [> solve_notGcd | solveTotient])
-      ||
-     (apply Totient'_Gcd1; [> solve_Gcd | solveTotient]))).
+     (apply Totient'_Gcd1; [> solve_Gcd | idtac])); apply Totient'_One.
+*)
 
+Ltac contradictT H := unfold notT in H; contradict H.
+
+Ltac solveTotient :=
+  let H := fresh "H"
+  in
+  match goal with
+  | [ |- Totient ?X ?Y ] =>
+      destruct (Gcd1_dec X Y) as [H |];
+        [> try (apply Totient'_Gcd1; [> (try solve_Gcd; try (contradictT H; solve_notGcd)) | idtac])
+         | try (apply Totient'_NotGcd1; [> (try solve_notGcd; try (contradictT H; solve_Gcd)) | idtac])]
+  end.
 
 Example Totient_9 : Totient 9 6.
-Proof. solveTotient.
+Proof. solveTotient. contradict H. solve_notGcd.  solveTotient.
 (* solveTotient. *)
   apply Totient'_NotGcd1; [> solve_notGcd | idtac].
   apply Totient'_Gcd1; [> solve_Gcd | idtac].
@@ -127,8 +192,6 @@ Proof. solveTotient.
   apply Totient'_NotGcd1; [> solve_notGcd | idtac].
   apply Totient'_Gcd1; [> solve_Gcd | idtac].
   apply Totient'_One.
-  apply Totient'_Gcd1; [> solve_Gcd | idtac].
-
 Defined.
 
 
