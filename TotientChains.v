7e868c841ef7c7ee91d4869aef398e6f458bba44
David Young <roboguy06@gmail.com>
2021-01-30 00:47:27 -0600

    Totient chains

--- TotientChains.v
+++ TotientChains.v
@@ -4,6 +4,10 @@ Require Import Coq.Arith.Wf_nat.
 Require Import Coq.Arith.Compare_dec.
 Require Import Coq.Arith.PeanoNat.
 Require Import Coq.Program.Wf.
+Require Import Ring.
+Require Import NArithRing.
+Require Import NArith.
+Require ArithRing.
 
 Inductive Parity := Even | Odd.
 
@@ -146,7 +150,7 @@ Defined.
 
 
 Inductive Totient' : nat -> nat -> nat -> Type :=
-| Totient'_One : forall x, Totient' 1 x 1
+| Totient'_One : forall x, Totient' 1 (S x) 1
 | Totient'_Gcd1 : forall d n t,
     Gcd (S d) n 1 -> Totient' d n t -> Totient' (S d) n (S t)
 | Totient'_NotGcd1 : forall d n t,
@@ -154,45 +158,239 @@ Inductive Totient' : nat -> nat -> nat -> Type :=
 
 Definition Totient n t := Totient' n n t.
 
-(*
-Fixpoint totient (d n : nat) : nat.
-Proof.
-  Check wf.
-*)
-
-(*
-Ltac solveTotient :=
-  repeat
-    ((apply Totient'_NotGcd1; [> solve_notGcd | idtac])
-      ||
-     (apply Totient'_Gcd1; [> solve_Gcd | idtac])); apply Totient'_One.
-*)
-
 Ltac contradictT H := unfold notT in H; contradict H.
 
 Ltac solveTotient :=
-  let H := fresh "H"
-  in
-  match goal with
-  | [ |- Totient ?X ?Y ] =>
-      destruct (Gcd1_dec X Y) as [H |];
-        [> try (apply Totient'_Gcd1; [> (try solve_Gcd; try (contradictT H; solve_notGcd)) | idtac])
-         | try (apply Totient'_NotGcd1; [> (try solve_notGcd; try (contradictT H; solve_Gcd)) | idtac])]
-  end.
+  repeat ((apply Totient'_One) ||
+          (apply Totient'_Gcd1; [> solve_Gcd | idtac]) ||
+          (apply Totient'_NotGcd1; [> solve_notGcd | idtac])).
 
 Example Totient_9 : Totient 9 6.
-Proof. solveTotient. contradict H. solve_notGcd.  solveTotient.
-(* solveTotient. *)
-  apply Totient'_NotGcd1; [> solve_notGcd | idtac].
-  apply Totient'_Gcd1; [> solve_Gcd | idtac].
-  apply Totient'_Gcd1; [> solve_Gcd | idtac].
-  apply Totient'_NotGcd1; [> solve_notGcd | idtac].
-  apply Totient'_Gcd1; [> solve_Gcd | idtac].
-  apply Totient'_Gcd1; [> solve_Gcd | idtac].
-  apply Totient'_NotGcd1; [> solve_notGcd | idtac].
-  apply Totient'_Gcd1; [> solve_Gcd | idtac].
-  apply Totient'_One.
+Proof. solveTotient. Defined.
+
+Lemma Totient_pos : forall {x y z}, Totient' x y z -> z > 0.
+Proof.
+  intros.
+  induction H; lia.
 Defined.
 
+Lemma Totient_unique : forall s s' x y z,
+  Totient' (S s) x y -> Totient' (S s') x z -> z = y.
+Proof.
+  dependent induction s using (well_founded_induction lt_wf); intros.
+  destruct s.
+  inversion H1; subst. inversion H0; subst; try easy.
 
-Lemma Totient_
+  inversion H0; subst; try easy.
+
+
+  pose (P := H _ _ _ _ _ _ H0 H1).
+
+
+
+  induction s, s'; intros.
+
+  inversion H; subst.
+
+
+  dependent induction s' using (well_founded_induction lt_wf); intros.
+  destruct s'. inversion H1.
+
+  inversion H0; subst.
+  inversion H1; subst; try easy.
+  assert (A0 : s' < S s'). lia.
+  pose (P := H _ A0 _ _ _ H0 H4).
+
+
+
+
+  dependent induction s using (well_founded_induction lt_wf); intros.
+  destruct s. inversion H0.
+
+  inversion H0; subst.
+  destruct s'. inversion H1.
+  pose (P := H _ _ _ _ _ _ H1 H0).
+
+  inversion H0; subst.
+  inversion H1; subst; try easy.
+
+  inversion H1; subst. inversion H4.
+  assert (A0 : x < S x). lia.
+  pose (P := H x A0 (S x) t0).
+
+
+Lemma Totient_exists : forall x, x > 0 -> { y & Totient x y }.
+Proof.
+  intros.
+
+  induction x using (well_founded_induction lt_wf).
+  dependent induction x. lia.
+
+
+  assert (A0 : forall y, y < x -> y > 0 -> {y0 & Totient y y0}). intros. apply H0; lia.
+
+  destruct x.
+
+  exists 1. solveTotient.
+
+  destruct IHx; try easy. lia.
+
+  destruct (Gcd1_dec (S x) (S (S x))).
+  exists (S x0).
+  apply Totient'_NotGcd1. solve_notGcd.
+  apply Totient'_Gcd1. assumption.
+
+  destruct (H0 (S x)); try lia.
+
+
+
+
+  destruct t.
+  inversion t; subst.
+
+
+
+
+
+  induction x.
+  exists 1. constructor.
+
+  destruct IHx. lia.
+  inversion t; subst.
+  exists 1. solveTotient.
+
+  destruct (Gcd1_dec x (S (S x))).
+  exists 
+
+
+
+
+
+
+
+
+
+  exists (S (S t0)).
+
+  assert (A0 : x = 0). destruct H1. inversion g; subst; lia.
+  subst. inversion H2.
+
+  exists x0.
+  unfold Totient.
+  apply Totient'_NotGcd1. intro. destruct H0. inversion g; subst; lia.
+
+  destruct x0. inversion H2; subst.
+
+  pose (P := Totient_pos t). lia.
+
+  assert (A0 : x > 0).
+    destruct x. contradictT H1. constructor. intuition. constructor. intuition.
+
+  destruct (Gcd1_dec ((S x)) ((S (S x)))).
+  apply Totient'_Gcd1. assumption.
+
+  inversion H2; subst.
+
+
+  inversion t; subst; try lia; try now intuition.
+
+
+
+
+
+
+  destruct t.
+  destruct x1. constructor.
+
+  inversion H2; subst.
+
+
+
+
+  destruct (Gcd1_dec (S (S x1)) (S (S (S x1)))).
+  apply Totient'_Gcd1. assumption.
+  
+(*
+  assert (A1 : x < 3).
+    induction x; try lia.
+    inversion H2; subst; try lia.
+    pose (P := Totient_pos H6); lia.
+    destruct x. inversion H4.
+    destruct x. lia.
+
+    assert (A1 : S (S x) > 0). lia.
+    pose (P := IHx H4 A1).
+    assert (A2 : x = 0). lia.
+    subst.
+    destruct x1.
+    contradictT H3. solve_Gcd.
+
+    dependent destruction H4.
+    pose (Totient_pos H4); lia.
+
+*)
+
+
+
+    inversion H2; subst. intuition.
+    inversion H6; subst. intuition.
+    inversion H8; subst.
+
+
+
+
+
+
+    contradict 
+    inversion H6; subst. pose (Totient_pos H10); lia.
+
+
+
+    generalize H3.
+    dependent induction x1 using (well_founded_induction lt_wf).
+
+
+
+
+ pose (Gcd_sym g). intuition.
+    inversion H4; subst. pose (Totient_pos H8). lia.
+    destruct H6.
+
+    induction H2; try lia.
+    assert (A1' : S x > 0). lia.
+    pose (P := IHx H4 A1'). lia.
+    
+
+
+
+ inversion H2; subst. lia.
+    pose (P := Totient_pos H6). lia. inversion H3; subst; try lia.
+    pose (P := Totient_pos H8). lia.
+    inversion H5; subst; try lia. inversion H3; subst; try lia.
+    pose (P := Totient_pos H10). lia.
+
+  apply .
+
+
+  destruct (Gcd1_dec x1 (S x1)).
+  apply Totient'_Gcd1.
+
+
+
+  destruct (Gcd1_dec (S x) (S (S x))).
+  apply Totient'_Gcd1. assumption.
+  destruct H2.
+  
+
+  inversion H2; subst.
+  inversion t; subst; try lia.
+
+
+
+  inversion H2; subst. 
+
+  inversion t; subst; try now inversion H2.
+
+  destruct (Gcd1_dec (S x) (S (S x))).
+  apply Totient'_Gcd1. assumption.
+(*  inversion t; subst; try now inversion H2. *)
